d17101a0dc75c391f0e0772c4fd15add7ffc0994
Арсений <115050782+ArPaSokolov@users.noreply.github.com>
2023-09-05 00:31:55 +0200

    lab 3 finished (py)

---
+++ lab_03/main.py
@@ -0,0 +1,11 @@
+x = int(input("Введите верхнюю границу: "))
+mas = []
+for c in range (0, 100):
+    for b in range(0, 100):
+        for a in range(0, 100):
+            t = 3 ** a * 5 ** b * 7 ** c
+            if t < x:
+                mas.append(t)
+            else:
+                break
+print("Числа:", sorted(mas))
