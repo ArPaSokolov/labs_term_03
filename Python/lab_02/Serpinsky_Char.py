b1dd5fbd4e19c47934b431aac73235ae505035af
Арсений <115050782+ArPaSokolov@users.noreply.github.com>
2023-10-01 23:17:28 +0200

    updated

--- Python/lab_02/Serpinsky_Char.py
+++ Python/lab_02/Serpinsky_Char.py
@@ -1,17 +1,17 @@
 n = int(input("Enter a depth of the Serpinsky triangle: "))
 
-n = (3 ** n) - 1 # каждые 3 ** n строчек в тр Паскаля = 1 уровень глубины в тр Серпинского
+n = (2 ** n) - 1 # каждые 2 ** n строчек в тр Паскаля = 1 уровень глубины в тр Серпинского
 
 p_triangle = [[1] + [0] * n for i in range(n + 1)]  # создаем матрицу под тр Паскаля
 s_triangle = [[""] * (n + 1) for x in range(n + 1)] # создаем матрицу под тр Серпинского
 
 for i in range(1, n + 1):
     for j in range(1, i + 1):
-        p_triangle[i][j] = (p_triangle[i - 1][j] + p_triangle[i - 1][j - 1]) % 3 # заполняем тр Паскаля 1, 2 и 0
+        p_triangle[i][j] = (p_triangle[i - 1][j] + p_triangle[i - 1][j - 1]) % 2 # заполняем тр Паскаля 1 и 0
 
 for i in range(0, n + 1): # заполняем матрицу для тр Серпинского
     for j in range(0, n + 1):
-        if p_triangle[i][j] == 1 or p_triangle[i][j] == 2: # любая единица или двойка в тр Паскаля = * в тр Серпинского
+        if p_triangle[i][j] == 1: # любая единица или двойка в тр Паскаля = * в тр Серпинского
             s_triangle[i][j] = "*"
         else:
             s_triangle[i][j] = " " # иначе - пробел
