2f2b0813155b1c5b3f4753fd7b226279666d57e1
Арсений <115050782+ArPaSokolov@users.noreply.github.com>
2023-10-01 15:21:02 +0200

    updated

--- Python/lab_01/task_02.py
+++ Python/lab_01/task_02.py
@@ -1,6 +1,11 @@
 n = int(input("Enter N: "))
-x = []
-for i in range(1, n+1):
-    x.append(i)
+x = str()
+for i in range(1, n + 1):
+    x += str(i)
     print(x)
 
+# n = int(input("Enter N: "))
+# for i in range(1, n+1):
+#     for j in range(1, i + 1):
+#         print(j, end=" ")
+#     print()
