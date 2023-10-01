47b07d9307200969b4eda157bbf8a82c019f7f02
Арсений <115050782+ArPaSokolov@users.noreply.github.com>
2023-10-01 15:19:52 +0200

    commented

--- Python/lab_02/Pascal.py
+++ Python/lab_02/Pascal.py
@@ -1,12 +1,11 @@
-n = int(input("Введите глубину: "))
+n = int(input("Enter a depth of the Pascal triangle: ")) # вводим количество строчек в треугольнике Паскаля
 
-
-triangle = [[1] + [0] * n for i in range(n + 1)]
+triangle = [[1] + [0] * n for i in range(n + 1)] # заполняем первый столбец матрицы N*N единицами, остальное - нулями
 
 for i in range(1, n + 1):
     for j in range(1, i + 1):
-        triangle[i][j] = triangle[i - 1][j] + triangle[i - 1][j - 1]
+        triangle[i][j] = triangle[i - 1][j] + triangle[i - 1][j - 1] # получаем текущий элемент из суммы двух стоящих сверху
 
 for i in triangle:
-    new_i = [j for j in i if j != 0]
-    print(*new_i, sep="\t")
+    new_i = [j for j in i if j != 0] # получаем новые строки без нулей
+    print(*new_i, sep="\t") # выводим тр Паскаля
--- Python/lab_02/Serpinsky_Char.py
+++ Python/lab_02/Serpinsky_Char.py
@@ -1,40 +1,20 @@
-# H = 34
-# W = 67
-#
-# M = [[' '] * W for _ in range(H)]
-#
-# M[1][W//2] = '*'
-# for i in range(H):
-#     for j in range(W):
-#         if j < (W - 1) and (M[i - 1][j - 1] == '*' and M[i - 1][j + 1] == ' ' or
-#                             M[i - 1][j + 1] == '*' and M[i - 1][j - 1] == ' '):
-#             M[i][j] = '*'
-#         if i == H - 1:
-#             M[i][j] = ' '
-# for i in range(H):
-#     for j in range(W):
-#         if (M[i][j - 1] == '*' and M[i][j + 1] == '*') and \
-#                 (M[i][j - 2] == ' ' and M[i][j + 2] == ' '):
-#             M[i][j] = '*'
-#
-# for x in M:
-#     print(*x)
-
 n = int(input("Enter a depth of the Serpinsky triangle: "))
 
-n = (3 ** n) - 1
-triangle = [[1] + [0] * n for i in range(n + 1)]
+n = (3 ** n) - 1 # каждые 3 ** n строчек в тр Паскаля = 1 уровень глубины в тр Серпинского
+
+p_triangle = [[1] + [0] * n for i in range(n + 1)]  # создаем матрицу под тр Паскаля
+s_triangle = [[""] * (n + 1) for x in range(n + 1)] # создаем матрицу под тр Серпинского
 
 for i in range(1, n + 1):
     for j in range(1, i + 1):
-        triangle[i][j] = (triangle[i - 1][j] + triangle[i - 1][j - 1]) % 3
+        p_triangle[i][j] = (p_triangle[i - 1][j] + p_triangle[i - 1][j - 1]) % 3 # заполняем тр Паскаля 1, 2 и 0
 
-for i in range(0, n + 1):
+for i in range(0, n + 1): # заполняем матрицу для тр Серпинского
     for j in range(0, n + 1):
-        if triangle[i][j] == 1 or triangle[i][j] == 2:
-            triangle[i][j] = "*"
+        if p_triangle[i][j] == 1 or p_triangle[i][j] == 2: # любая единица или двойка в тр Паскаля = * в тр Серпинского
+            s_triangle[i][j] = "*"
         else:
-            triangle[i][j] = " "
+            s_triangle[i][j] = " " # иначе - пробел
 
-for x in triangle:
+for x in s_triangle: # выводим тр Серпинского
     print(*x)
